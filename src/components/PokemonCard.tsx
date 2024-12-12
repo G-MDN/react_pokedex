36512865bd72fa1d09e2ebc2af8ea0f7aad48d9f
G-MDN <gregoiremidon@gmail.com>
2024-12-12 17:19:07 +0100

    solution quete4

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,17 +1,5 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+function PokemonCard({pokemon}) {
 
-function PokemonCard() {
-
-    const pokemon = pokemonList[0]
     return (
     <figure>
         {
@@ -21,8 +9,6 @@ function PokemonCard() {
         <figcaption>{pokemon.name}</figcaption>
         </figure>
     )
-  }
-  
-  export default PokemonCard;
+}
 
-  
+export default PokemonCard;
