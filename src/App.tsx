ddcf6f0503fb3bee033b3f28e57db00694c65202
G-MDN <gregoiremidon@gmail.com>
2025-01-02 10:36:54 +0100

    Solution quete 7

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,21 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
@@ -16,29 +31,21 @@ const pokemonList = [
 function App() {
   const [pokemonIndex, setPokemonIndex] = useState(0);
 
-  const next = () => {
-    setPokemonIndex(pokemonIndex < pokemonList.length-1 ?
-        pokemonIndex +1: 
-        pokemonIndex
-    );
-  };
-
-  const prev = () => {
-    setPokemonIndex(pokemonIndex >0 ?
-      pokemonIndex -1: 
-      pokemonIndex
-    );
-  }
   return (
-    <div>
+    <section>
       <PokemonCard pokemon={pokemonList[pokemonIndex]} />
-      <button type="button" onClick={prev}>
-        Précédent
-      </button>
-      <button type="button" onClick={next}>
-        Suivant
-      </button>
-    </div>
+      <nav>
+        {pokemonList.map((pokemon, index) => (
+          <button
+            key={pokemon.name}
+            type="button"
+            onClick={() => setPokemonIndex(index)}
+          >
+            {pokemon.name}
+          </button>
+        ))}
+      </nav>
+    </section>
   );
 }
 
